9f70249a288144e64b03e6af112a1147f841b410
Ilya Azin <martis.azin@gmail.com>
2022-06-10 05:25:28 +0300

    fix(fixtures): add more fixtures

--- src/shared/fixtures/index.ts
+++ src/shared/fixtures/index.ts
@@ -1,25 +1,9 @@
-import PRESET_GH_FDD from "./imports.gh-fdd.json";
-
-// const FIXTURES_CUTS = Object.entries(FIXTURES).reduce((acc, [fKey, fImp]) => {
-//     return { ...acc, [fKey]: asAbsFile(fImp).split("/")};
-// }, {})
-
-function __asAbsFile(file: TFile): TFile {
-    // NOTE: normalize truncating
-    // const files = Object.keys(importsBaseGraph).map((file) => file.replaceAll("../", "").replace("./", ""));
-    return file.replace("../", "");
-}
-
-// !!! FIXME: Починить парсинг импортов, вместо клина
-// NOTE: replace to native replaceAll
-function __cleanImports(imports: ImportsGraph): ImportsGraph {
-    return Object.entries(imports).reduce((acc, [file, fileDeps]) => {
-        return { ...acc, [__asAbsFile(file)]: fileDeps.map(__asAbsFile) };
-    }, {});
-}
+import PRESET_GH_FDD from "./gh-fdd.imports.json";
+import PRESET_GH_FDD_APP from "./gh-fdd.imports-app.json";
+import PRESET_FAVEIN from "./favein.imports.json";
 
 export const _GH_FDD = {
-    imports: __cleanImports(PRESET_GH_FDD) as ImportsGraph,
+    imports: PRESET_GH_FDD as ImportsGraph,
     files: {
         SH_GET_ENV: "shared/get-env/index.ts",
         FE_AUTH_HOOKS: "features/auth/hooks.ts",
@@ -29,4 +13,14 @@ export const _GH_FDD = {
         // FIXME: feature/auth#useAuth usage detecting 
         HEADER: "app/header/index.tsx",
     }
+} //?
+
+export const _GH_FDD__APP = {
+    imports: PRESET_GH_FDD_APP as ImportsGraph,
+    files: _GH_FDD.files,
+} //?
+
+export const _FAVEIN = {
+    imports: PRESET_FAVEIN as ImportsGraph,
+    files: {},
 } //?
